2c42e9c6f83af98f281eddada75ead8ceda96f19
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-25 00:26:36 +0100

    switch to gruvbox

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -1,17 +1,8 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
-
--- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
---
--- In your plugin files, you can:
--- * add extra plugins
--- * disable/enabled LazyVim plugins
--- * override the configuration of LazyVim plugins
-return {
+if true then return {
   -- add gruvbox
   { "ellisonleao/gruvbox.nvim" },
-
   -- Configure LazyVim to load gruvbox
   {
     "LazyVim/LazyVim",
@@ -20,6 +11,15 @@ return {
     },
   },
 
+} end
+
+-- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+--
+-- In your plugin files, you can:
+-- * add extra plugins
+-- * disable/enabled LazyVim plugins
+-- * override the configuration of LazyVim plugins
+return {
   -- change trouble config
   {
     "folke/trouble.nvim",
