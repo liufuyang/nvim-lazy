a38f4c2aca338d933abacb32b74d6c27c27ba01d
Fuyang Liu <liufuyang@users.noreply.github.com>
2024-01-25 01:36:14 +0100

    only trigger auto completion manually

--- lua/plugins/example.lua
+++ lua/plugins/example.lua
@@ -10,6 +10,13 @@ if true then return {
       colorscheme = "gruvbox",
     },
   },
+  {
+    "hrsh7th/nvim-cmp",
+    ---@param opts cmp.ConfigSchema
+    opts = function(_, opts)
+      opts.completion.autocomplete = false
+    end,
+  },
 
 } end
 
